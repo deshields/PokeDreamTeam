cdcf566ad4e44f5153ee3ad259aa0205eefbcaab
Ashley Deshields <adeshiel@bu.edu>
2018-05-29 23:23:46 -0400

    fixed looping in move scraper

--- MoveScraper.py
+++ MoveScraper.py
@@ -1,12 +1,14 @@
 from urllib.request import Request, urlopen
 import csv
 from bs4 import BeautifulSoup
+import pandas as pd
 
 all_page = "statuses/All Moves.html"
 all = open(all_page)
 all_soup = BeautifulSoup(all, 'html5lib')
 all_table = all_soup.find_all("table", class_="sortable")[0]
-atr = all_table.find_all("tr")[1:]
+atr = all_table.find_all("tr")[1:] #atr is a List
+#print(atr[1])
 
 sleep_page = "statuses/Sleep.html"
 sleep = open(sleep_page)
@@ -31,32 +33,49 @@ ptr = para_table.find_all("tr")[1:]
 freeze_page = "statuses/Freeze.html"
 freeze = open(freeze_page)
 freeze_soup = BeautifulSoup(freeze, 'html5lib')
-freeze_table = free_soup.find_all("table", class_="sortable")[0]
+freeze_table = freeze_soup.find_all("table", class_="sortable")[0]
 ftr = freeze_table.find_all("tr")[1:]
 
 poison_page = "statuses/Poison.html"
 poison = open(poison_page)
 poison_soup = BeautifulSoup(poison, 'html5lib')
 poison_table = poison_soup.find_all("table", class_="sortable")[0]
-ptr = poison_table.find_all("tr")[1:]
+potr = poison_table.find_all("tr")[1:]
 bad_poison_table = poison_soup.find_all("table", class_="sortable")[1]
 bptr = bad_poison_table.find_all("tr")[1:]
 
 # TODO: add the other effects before the write statement
 
 
-with open("status_causing_moves.csv", 'w') as init:
-    hdrs = ["Move", "No.", "Type", "Category", "Power", "PP", "Accuracy", "Effect", "Notes"] # Effect => ["Freeze", 6.67]
-    writer = csv.DictWriter(csvfile, fieldnames=hdrs)
-    writer.writeheader()
+# with open("status_causing_moves.csv", 'w') as init:
+#     hdrs = ["Name", "No.", "Type", "Category", "Power", "PP", "Accuracy", "Effect", "Notes"] # Effect => ["Freeze", 6.67]
+#     writer = csv.DictWriter(init, fieldnames=hdrs)
+#     writer.writeheader()
+#
+#     # Scan all moves first
+    # for tr in atr:
+# tds = atr[1].find_all('td') # this works
+for tr in range(1, len(atr)):
+    tds = atr[tr].find_all('td')
+    print("Result: ", tds[4].text)
+#         dict = {"Name": tds[1].text, "No.": tds[0].text, "Type": tds[2].text, "Category":tds[3].text, "Power": tds[6].text, "PP": tds[5].text, "Accuracy":tds[7].text, "Effect": [] }
+#         writer.writerow(dict)
+#
+#     #TODO: check for status-inducing moves and update accordingly
+#
+# df = pd.read_csv("status_causing_moves.csv", index_col=0)
+#
+# for t1, t2, t3, t4, t5, t6 in izip(str, btr, ptr, ftr, potr, bptr):
+#     tds1, tds2, tds3, tds4, tds5, tds6 = t1.find_all('td'), t2.find_all('td'), t3.find_all('td'), t4.find_all('td'), t5.find_all('td'), t6.find_all('td')
+#     df.loc[tds1[0].text]["Effect"] = ["SLEEP", float(tds1[3].text.strip('%'))/100]
+#     df.loc[tds2[0].text]["Effect"] = ["BURNED", float(tds2[3].text.strip('%'))/100]
+#     df.loc[tds3[0].text]["Effect"] = ["PARALYZED", float(tds3[3].text.strip('%'))/100]
+#     df.loc[tds4[0].text]["Effect"] = ["FROZEN", float(tds4[3].text.strip('%'))/100]
+#     df.loc[tds5[0].text]["Effect"] = ["POISONED", float(tds5[3].text.strip('%'))/100]
+#     df.loc[tds6[0].text]["Effect"] = ["BADLY POISONED", float(tds6[3].text.strip('%'))/100]
 
-    # Scan all moves first
-    for tr in atr:
-        tds = tr.find_all('td')
-        dict = {"Move": tds[1], "No.": tds[0], "Type": tds[2], "Category":tds[3], "Power":tds[6], "PP":tds[5], "Accuracy":tds[7] }
-        writer.writerow(dict)
 
-
-
-        print("Move: %s Type: %s Category: %s" % \
-            (tds[0].text, tds[1].text, tds[2].text))
+        #
+        #
+        # print("Move: %s Type: %s Category: %s" % \
+        #     (tds[0].text, tds[1].text, tds[2].text))
